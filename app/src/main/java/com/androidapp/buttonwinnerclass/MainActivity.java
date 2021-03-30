ba8562ca26e9cfcdad825865d6ff359cce795acf
C-H Han <itq1@naver.com>
2021-03-30 11:32:35 +0900

    added 2nd button with new inner class

--- app/src/main/java/com/androidapp/buttonwinnerclass/MainActivity.java
+++ app/src/main/java/com/androidapp/buttonwinnerclass/MainActivity.java
@@ -3,11 +3,12 @@ package com.androidapp.buttonwinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button mButton1;
+    Button mButton1, mButton2;
     TextView mTextView1;
 
 
@@ -17,8 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton1 = (Button)findViewById(R.id.button1);
+        mButton2 = (Button)findViewById(R.id.button2);
+
         mTextView1 = (TextView)findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener(this));
+        mButton1.setOnClickListener(new MyOnClickListener());
+        mButton2.setOnClickListener(new MyOnClickListener2());
+
+    }
+
+    class MyOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v){
+            mTextView1.setText("You clicked CH's button!");
+        }
+    }
+
+    private class MyOnClickListener2 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            mTextView1.setText("You clicked CH's 2nd button!");
+
+        }
     }
 }
