bf31c04a3d3d04ed60d33f6ff913db353ea320b8
C-H Han <itq1@naver.com>
2021-03-30 10:50:56 +0900

    inner class without this

--- app/src/main/java/com/androidapp/buttonwinnerclass/MainActivity.java
+++ app/src/main/java/com/androidapp/buttonwinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package com.androidapp.buttonwinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         mButton1 = (Button)findViewById(R.id.button1);
         mTextView1 = (TextView)findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener(this));
+        mButton1.setOnClickListener(new MyOnClickListener()); //this가 같은 화면에 있기 때문에 다 빼버
+    }
+    private class MyOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v){
+            mTextView1.setText("You clicked CH's button!");
+        }
     }
 }
